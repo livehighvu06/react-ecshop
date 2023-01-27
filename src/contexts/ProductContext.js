fef7c2a8aff60eca03470d3d2d9e17b5949439d4
Eddie Chen <livehighvu06@gmail.com>
2023-01-28 03:20:50 +0800

    定義Product Context / Home父組件 / Product子組件

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { createContext, useState, useEffect } from "react";
 
-const ProductContext = () => {
-  return <div>ProductContext</div>;
+export const ProductContext = createContext();
+
+const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("https://fakestoreapi.com/products");
+      const data = await res.json();
+      // console.log(data);
+      setProducts(data);
+    };
+    fetchData();
+  }, []);
+  return (
+    <ProductContext.Provider value={{ products }}>
+      {children}
+    </ProductContext.Provider>
+  );
 };
 
-export default ProductContext;
+export default ProductProvider;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useContext } from "react";
+import Product from "../components/Product";
+import { ProductContext } from "../contexts/ProductContext";
 
 const Home = () => {
-  return <div>Homepage</div>;
+  const { products } = useContext(ProductContext);
+  const filteredProducts = products.filter((item) => {
+    return (
+      item.category === "men's clothing" || item.category === "women's clothing"
+    );
+  });
+  return (
+    <div>
+      <section>
+        <div className="container mx-auto">
+          <div className="grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:lg:grid-cols-5">
+            {filteredProducts.map((product) => {
+              return <Product key={product.id} product={product} />;
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
