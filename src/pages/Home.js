b8921372ad58302abd53b44aaa0741705d6f662e
Eddie Chen <livehighvu06@gmail.com>
2023-01-29 03:58:58 +0800

    追加篩選功能

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Product from "../components/Product";
 import Hero from "../components/Hero";
+import Filter from "../components/Filter";
 import Loading from "../components/Loading";
 import { ProductContext } from "../contexts/ProductContext";
 
 const Home = () => {
   const { products } = useContext(ProductContext);
-  console.log(products);
-  const filteredProducts = products.filter((item) => {
-    return (
-      item.category === "men's clothing" || item.category === "women's clothing"
-    );
+  const [filtered, setFiltered] = useState([]);
+
+  const renderedProducts = filtered.map((product) => {
+    return <Product key={product.id} product={product} />;
   });
 
   return (
@@ -18,13 +18,14 @@ const Home = () => {
       <Hero />
       <section className="py-16">
         <div className="container mx-auto">
+          <div>
+            <Filter setFiltered={setFiltered} products={products} />
+          </div>
           {products.length === 0 ? (
             <Loading />
           ) : (
             <div className="grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:lg:grid-cols-5">
-              {filteredProducts.map((product) => {
-                return <Product key={product.id} product={product} />;
-              })}
+              {renderedProducts}
             </div>
           )}
         </div>
