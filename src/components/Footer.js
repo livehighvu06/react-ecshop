3eef5de8bfea5f05a15c7cd5d1f83d87fcf7e350
Eddie Chen <livehighvu06@gmail.com>
2023-01-28 18:28:26 +0800

    定義Footer樣式

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-primary py-6">
+      <div className="container mx-auto">
+        <p className="text-white text-center text-sm">
+          Copyright &copy; 個人練習
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
